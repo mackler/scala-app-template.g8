05b80bd9b64e065e3bedb42281c5cd235274d5d2
Adam Maklér <dev@upisom.sk>
2024-11-30 19:08:32 +0100

    Change tests to use ScalaTest + ScalaCheck integration

--- src/main/g8/build.sbt
+++ src/main/g8/build.sbt
@@ -12,5 +12,6 @@ $if(use_zio.truthy)$
 $else$
   "org.typelevel" %% "cats-effect" % "$cats_version$",
 $endif$
-  "org.scalacheck" %% "scalacheck" % "$scalacheck_version$" % Test,
+  "org.scalatest" %% "scalatest" % "3.2.19" % "test",
+  "org.scalatestplus" %% "scalacheck-1-18" % "3.2.19.0" % Test,
 )
--- src/main/g8/src/test/scala/MyProps.scala
+++ src/main/g8/src/test/scala/MyProps.scala
@@ -1,6 +1,14 @@
-import org.scalacheck.Properties
-import org.scalacheck.Prop.forAll
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.prop.TableFor2
+import org.scalatest.propspec.AnyPropSpec
+import org.scalatestplus.scalacheck.ScalaCheckPropertyChecks
 
-object MyProps extends Properties("MyProps"):
-  property("myProp1") = forAll: (n: Int, m: Int) =>
-    n + m == m + n
+class MyProps extends AnyPropSpec with ScalaCheckPropertyChecks with Matchers:
+
+  property("commutativity of integer addition"):
+    forAll: (n: Int, m: Int) =>
+      (n + m) should equal (m + n)
+
+  property("addition without a generator"):
+    forAll(TableFor2(("int", "int"), (2, 2))): (a, b) =>
+      (a + b) should equal (4)
